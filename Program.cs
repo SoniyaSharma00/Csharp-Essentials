6de0091e2dd3e0dae05b490f7837f9fc04959d02
Soniya Sharma <soniyasharma2061@gmail.com>
2025-04-14 21:35:52 +0545

    Variations of output syntax

--- Program.cs
+++ Program.cs
@@ -13,8 +13,8 @@ namespace Csharp_Syntax
             Console.WriteLine("HELLO!");
             Console.WriteLine("Hello How are you?"); //This is the syntax for output in C#.
             Console.Write("This is"); // Console.WriteLine prints the output on the existing line and appends a new line after it.
-            Console.Write(" C# tutorial");//while Console.Writeline prints the output but does not append a new line after it.
-            Console.Write(" for students"); //hence Console.Writeline prints the next output in the same line.
+            Console.Write(" C# tutorial");//while Console.Write prints the output but does not append a new line after it.
+            Console.Write(" for students"); //hence Console.Write prints the next output in the same line.
         }
     }
 }
